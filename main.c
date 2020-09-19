dafa5c71da17e979feebac448427e361364241ff
Nipun Singal <nipunsingal7@gmail.com>
2020-09-19 20:08:24 +0530

    choice screen added

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-
+int choice=0;
 void display()
 {printf("\n\n\n\n\n\t\t\t\t\t\t\t\t    Welcome To\t\t\t\t\t\t\t\t\t");
  printf("\n\n\n\n\t\t\t\t\t\t\tHIGH DELIVERABLE PASS-CODE SYSTEM\t\t\t\t\t\t\t");
@@ -11,10 +11,33 @@ void display()
 
 }
 
+void display2()
+{printf("\n\n\n\tPlease choose the option:");
+ printf("\n\n\t1)Only encryption\n\t2)Encryption + Hashing\n\t3)Triple Crypt\n\t4)Exit\n\n\tnote:It is recomended to use Triple Crypt for high security");
+ 
+ while(choice!=4)
+{printf("\n\n\n\tEnter your choise: ");
+ scanf("%d",&choice);
+ switch (choice)
+{case 1:
+        break;
+ case 2:
+         break;
+ case 3:
+         break;
+ case 4: exit(0);  
+         break; 
+ default: printf("invalid choice, select again"); 
+          break;
+
+} 
+}
+}
 
 int main()
 {char a;
  display();
 scanf("%c",&a);
 system("clear");
+display2();
  return(0);}
