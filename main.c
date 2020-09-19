b45f39fcd26c807b5c8097b6439381ec18a26f20
Nipun Singal <nipunsingal7@gmail.com>
2020-09-19 20:01:11 +0530

    main file

---
+++ main.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+void display()
+{printf("\n\n\n\n\n\t\t\t\t\t\t\t\t    Welcome To\t\t\t\t\t\t\t\t\t");
+ printf("\n\n\n\n\t\t\t\t\t\t\tHIGH DELIVERABLE PASS-CODE SYSTEM\t\t\t\t\t\t\t");
+ printf("\n\n\n\n\n\n\n\n\t\t\t\t\t\t  Don't worry we will secure passwords for you\t\t\t\t\t\t"); 
+ printf("\n\n\n\n\n\n\n\n\n\n\tDeveloped By:\n\tNipun Singal\n\tMadhav Bhatia\n\tRajat Panwar\n\tIshita Bansal");
+ printf("\n\n\t\t\t\t\t\t\t\t\t\t\t\t\tPress enter to continue......");
+
+}
+
+
+int main()
+{char a;
+ display();
+scanf("%c",&a);
+system("clear");
+ return(0);}
